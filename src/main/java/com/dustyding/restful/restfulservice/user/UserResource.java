2b89e967f32598ee2071686b40320f260a400a82
Ding-x <xiao_peng0202@hotmail.com>
2019-07-16 10:10:18 -0400

    hateoas

--- src/main/java/com/dustyding/restful/restfulservice/user/UserResource.java
+++ src/main/java/com/dustyding/restful/restfulservice/user/UserResource.java
@@ -1,13 +1,17 @@
 package com.dustyding.restful.restfulservice.user;
 
+import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+
 import java.net.URI;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -22,11 +26,16 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User findOne(@PathVariable int id){
+    public EntityModel<User> findOne(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null)
             throw new UserNotFoundException("id-"+id);
-        return user;
+
+
+        EntityModel<User> model = new EntityModel<User>(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).allUsers());
+        model.add(linkTo.withRel("all-users"));
+        return model;
     }
 
     @PostMapping("/users")
